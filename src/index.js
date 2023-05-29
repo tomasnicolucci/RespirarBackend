0b9e9e52a8b7b80e8a7e37dc82f7ae7960811ef1
tomasnicolucci <tomas.nicolucci@gmail.com>
2023-05-29 19:52:36 -0300

    Cambios en configuraciones

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 const app = express()
 
-app.use(require('./routes'));
+app.use(require('../routes/estaciones'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(3004, () => console.log('listening on port 3004'));
