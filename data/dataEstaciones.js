9a874bcaa2a0db7596fabeb71310e4b03b22ddcc
tomasnicolucci <tomas.nicolucci@gmail.com>
2023-05-30 02:08:36 -0300

    Nuevos endpoints

--- data/dataEstaciones.js
+++ data/dataEstaciones.js
@@ -3,14 +3,31 @@ const axios = require('axios');
 async function getEstaciones(){
     try{
         const estaciones = await axios.get("http://localhost:1026/v2/entities")
-        
         return estaciones.data
     }
     catch(error){
         console.log('Error get estaciones')
         return null
-    }
-    
+    }  
 }
 
-module.exports = {getEstaciones}
+async function getEstacionById(id){
+    try{
+        const estacion = await axios.get("http://localhost:1026/v2/entities/" + id)
+        return estacion.data
+    }catch(error){
+        console.log('Error get estacion por id')
+        return null
+    }  
+}
+
+async function getAtributos(id){
+    try{
+        const temp = await axios.get("http://localhost:1026/v2/entities/" + id + "/attrs")
+        return temp.data
+    }catch(error){
+        return null
+    }
+}
+
+module.exports = {getEstaciones, getEstacionById, getAtributos}
