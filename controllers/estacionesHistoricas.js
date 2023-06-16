2da9290a9a9019ae32b862e37b2377d547f685fd
tomasnicolucci <tomas.nicolucci@gmail.com>
2023-06-16 01:21:04 -0300

    Cambios en los endpoints de datos historicos

--- controllers/estacionesHistoricas.js
+++ controllers/estacionesHistoricas.js
@@ -8,12 +8,4 @@ async function getHistoricosByIdFecha(id, attr, minDate, maxDate) {
     return datosHistoricos.getHistoricosByIdFecha(id, attr, minDate, maxDate);
 }
 
-async function exportCsv(id, attr) {
-    return datosHistoricos.exportCsv(id, attr);
-}
-
-async function exportExcel(id, attr) {
-    return datosHistoricos.exportExcel(id, attr);
-}
-
-module.exports = {getHistoricosById, getHistoricosByIdFecha, exportCsv, exportExcel}
+module.exports = {getHistoricosById, getHistoricosByIdFecha}
--- routes/estacionesHistoricas.js
+++ routes/estacionesHistoricas.js
@@ -12,15 +12,4 @@ router.get('/getHistoricosByIdFecha/', async (req, res) => {
     res.json(await controllerHistoricos.getHistoricosByIdFecha(id, attr, minDate, maxDate));
 })
 
-//Exportacion datos
-router.get('/exportCsv/', async (req, res) => {
-    const {id, attr} = req.query
-    res.json(await controllerHistoricos.exportCsv(id, attr));
-})
-
-router.get('/exportExcel/', async (req, res) => {
-    const {id, attr} = req.query
-    res.json(await controllerHistoricos.exportExcel(id, attr));
-});
-
 module.exports = router
